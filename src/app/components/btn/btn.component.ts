f7a3da300055fe09a0ed1a87bc89c8be960b2fdd
Dror Handler <hdror507@gmail.com>
2024-06-08 15:40:08 +0300

    create btn

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-btn',
   templateUrl: './btn.component.html',
-  styleUrls: ['./btn.component.scss']
+  styleUrls: ['./btn.component.scss'],
+  
 })
 export class BtnComponent implements OnInit {
+
+  @Input() label!: string
 
   constructor() { }
 
