3ecae502a61b11c6e2a5426a69b22ee9d4883a9a
Dror Handler <hdror507@gmail.com>
2024-06-09 04:52:38 +0300

    clear code

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -4,35 +4,36 @@ import { merge } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  selector: 'app-form', 
+  templateUrl: './form.component.html', 
+  styleUrl: './form.component.scss' 
 })
 
 export class FormComponent {
+  // Define a FormControl for the name field with a required validator
+  name = new FormControl('', [Validators.required]);
 
-  name = new FormControl('', [Validators.required])
   errorMessage = '';
 
   constructor() {
+    // Combine the statusChanges and valueChanges observables of the name FormControl
     merge(this.name.statusChanges, this.name.valueChanges)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateErrorMessage());
+      .pipe(takeUntilDestroyed()) // Automatically unsubscribe when the component is destroyed
+      .subscribe(() => this.updateErrorMessage()); // Update the error message whenever the control's status or value changes
   }
 
-
+  // Method to update the error message based on the validation state of the name control
   updateErrorMessage() {
     if (this.name.hasError('required')) {
+      // Set the error message if the name control is invalid due to being required
       this.errorMessage = 'You must enter a name';
     } else {
+      // Clear the error message if there are no validation errors
       this.errorMessage = '';
     }
   }
 
-
-
   onSubmitForm() {
     console.log(this.name);
   }
-
 }
